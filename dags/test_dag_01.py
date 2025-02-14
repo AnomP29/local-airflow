26c76e56f078ca7ccf45de95350ca8227590b610
AnomP29 <49397814+AnomP29@users.noreply.github.com>
2025-02-14 11:36:59 +0700

    Update test_dag_01.py

--- dags/test_dag_01.py
+++ dags/test_dag_01.py
@@ -4,6 +4,7 @@ from datetime import datetime, timedelta
 
 import pandas as pd
 # import gspread
+from ruamel.yaml import YAML
 
 
 # df = pd.DataFrame([[1, 2], [3, 4], [5, 6], [7, 8]], columns=["A", "B"])
