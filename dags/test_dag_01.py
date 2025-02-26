70312f64735a2c89e1be7a85a11ec051dda8d3ca
AnomP29 <49397814+AnomP29@users.noreply.github.com>
2025-02-26 23:16:48 +0700

    Update test_dag_01.py

--- dags/test_dag_01.py
+++ dags/test_dag_01.py
@@ -11,7 +11,7 @@ from ruamel.yaml import YAML
 
 # df = pd.DataFrame([[1, 2], [3, 4], [5, 6], [7, 8]], columns=["A", "B"])
 # df = df.astype(str)
-# print(df)
+# print(df) asdfasdf
 
 default_args = {
     'owner': 'anomp',
