36d5dd658d7d8466b984497590c408dd7b69ba3b
anom <anom.prasetyo@sinbad.co.id>
2025-05-22 13:38:23 +0700

    a

--- dags/spark-job/sp-job-01.py
+++ dags/spark-job/sp-job-01.py
@@ -5,12 +5,19 @@ def main():
         .appName("MyApp") \
         .enableHiveSupport() \
         .config("hive.metastore.uris", "thrift://172.18.0.5:9083") \
+        .config("spark.hadoop.fs.s3a.endpoint", "http://172.18.0.4:9000") \
+        .config("spark.hadoop.fs.s3a.access.key", "minioadmin") \
+        .config("spark.hadoop.fs.s3a.secret.key", "minioadmin") \
+        .config("spark.hadoop.fs.s3a.path.style.access", "true") \
+        .config("spark.hadoop.fs.s3a.connection.ssl.enabled", "false") \
+        .config("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem") \
         .getOrCreate()
-
+    
     print("=== DATABASES ===")
     spark.sql("SHOW DATABASES").show()
     
-    # Show Hive tables
+    print("=== TABLES IN DEFAULT DATABASE ===")
+    spark.sql("USE default")    
     spark.sql("SHOW TABLES").show()
 
     # Optional: stop the session
