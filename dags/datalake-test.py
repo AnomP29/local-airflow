766fc90b80e14df4b771d04c06bf945c4ee27980
AnomP29 <49397814+AnomP29@users.noreply.github.com>
2025-05-22 13:03:33 +0700

    Update datalake-test.py

--- dags/datalake-test.py
+++ dags/datalake-test.py
@@ -11,7 +11,7 @@ from pathlib import Path
 # os.chdir('/opt/airflow/dags/repo/dags')
 # airflow_home = Path(os.environ.get("AIRFLOW_HOME", "/opt/airflow/dags/repo/dags/spark_job"))
 # sp_dir = airflow_home / "spark_job"
-spark_dir = str(Path('/opt/airflow/dags/repo/dags'))
+spark_dir = Path('/opt/airflow/dags/repo/dags')
 
 default_args = {
     'owner': 'anomp',
@@ -65,9 +65,8 @@ with DAG(
         --conf spark.hadoop.fs.s3a.connection.ssl.enabled=false \
         --conf spark.hadoop.fs.s3a.impl=org.apache.hadoop.fs.s3a.S3AFileSystem \
         {spark_dir}/spark_job/sp-job-01.py
-        """.format(spark_dir = spark_dir)
-    )
-    
+        """
+    )    
     task1 = EmptyOperator(task_id="task1")
 
     end = EmptyOperator(task_id="end")
