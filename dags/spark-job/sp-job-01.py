1e96a98099c8231f5ba966f11f826fdee3294981
AnomP29 <49397814+AnomP29@users.noreply.github.com>
2025-05-24 17:27:11 +0700

    Update sp-job-01.py

--- dags/spark-job/sp-job-01.py
+++ dags/spark-job/sp-job-01.py
@@ -1,17 +1,8 @@
 from pyspark.sql import SparkSession
 
 def main():
-    # spark = SparkSession.builder \
-    #     .appName("MyApp") \
-    #     .enableHiveSupport() \
-    #     .config("hive.metastore.uris", "thrift://172.18.0.4:9083") \
-    #     .config("spark.hadoop.fs.s3a.endpoint", "http://172.18.0.2:9000") \
-    #     .config("spark.hadoop.fs.s3a.access.key", "minioadmin") \
-    #     .config("spark.hadoop.fs.s3a.secret.key", "minioadmin") \
-    #     .config("spark.hadoop.fs.s3a.path.style.access", "true") \
-    #     .config("spark.hadoop.fs.s3a.connection.ssl.enabled", "false") \
-    #     .config("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem") \
-    #     .getOrCreate()
+    spark = SparkSession.builder \
+    .getOrCreate()
     
     print("=== DATABASES ===")
     sdb = spark.sql("SHOW DATABASES")
