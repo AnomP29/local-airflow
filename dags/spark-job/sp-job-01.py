620fdde62a8b9ead5ae243754f1ed4ed4861dfbb
AnomP29 <49397814+AnomP29@users.noreply.github.com>
2025-05-24 17:39:33 +0700

    Update sp-job-01.py

--- dags/spark-job/sp-job-01.py
+++ dags/spark-job/sp-job-01.py
@@ -8,16 +8,32 @@ def main():
         .getOrCreate()
 
 
-    print("=== DATABASES ===")
-    sdb = spark.sql("SHOW DATABASES")
-    odb = sdb._jdf.showString(20, 20, False)
-    print(odb)
+    sp_sql = '''
+    select 
+    --*
+    pickup_date 
+    ,count(1)
+    from hive."default".nyc_taxi_trip_yellow ntty
+    where 9=9
+    --and cast(ntty.pickup_date as varchar) = '2021-01-30'
+    group by 1
+    order by 1
+    --limit 1
+    ;
+    '''
+    q01 = spark.sql(sp_sql)
+    oq01 = q01._jdf.showString(20, 20, False)
+    print(q01)
+    # print("=== DATABASES ===")
+    # sdb = spark.sql("SHOW DATABASES")
+    # odb = sdb._jdf.showString(20, 20, False)
+    # print(odb)
     
-    print("=== TABLES IN DEFAULT DATABASE ===")
+    # print("=== TABLES IN DEFAULT DATABASE ===")
     # spark.sql("USE default")    
-    stb = spark.sql("SHOW TABLES")
-    otb = stb._jdf.showString(20, 20, False)
-    print(otb)
+    # stb = spark.sql("SHOW TABLES")
+    # otb = stb._jdf.showString(20, 20, False)
+    # print(otb)
     
     # Optional: stop the session
     spark.stop()
