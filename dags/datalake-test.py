2a291e71c44d29e8369a0425b779b28fe284bb9b
AnomP29 <49397814+AnomP29@users.noreply.github.com>
2025-05-24 22:51:50 +0700

    Update datalake-test.py

--- dags/datalake-test.py
+++ dags/datalake-test.py
@@ -20,26 +20,6 @@ default_args = {
     'catchup': False
 }
 
-
-def spark_builder():
-    print('a')
-    builder = SparkSession.builder \
-        .appName("MyApp") \
-        .master("spark://172.18.0.6:7077") \
-        .enableHiveSupport() \
-        .config("spark.hadoop.fs.s3a.endpoint", "http://172.18.0.2:9000") \
-        .config("spark.hadoop.fs.s3a.access.key", "minioadmin") \
-        .config("spark.hadoop.fs.s3a.secret.key", "minioadmin") \
-        .config("spark.hadoop.fs.s3a.path.style.access", "true") \
-        .config("spark.hadoop.fs.s3a.addressing.style", "path") \
-        .config("spark.hadoop.fs.s3a.connection.ssl.enabled", "false") \
-        .config("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
-    # spark = builder.getOrCreate()
-    # spark.sql("SHOW TABLES").show()
-    # return spark.sql("SHOW TABLES").show()
-
-
-
 with DAG(
     'Datalake-Test',
     default_args = default_args,
